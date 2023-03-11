2bde84aaf6f140b046b0679cd9f4765e30d3add8
SahebHassan <124240923+SahebHassan@users.noreply.github.com>
2023-03-11 06:05:08 +0000

    practice boxes topic

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
-console.log('hello')
-let titleDiv=document.getElementById('title')
-console.log('before: ',titleDiv.innerText)
-let message='Goodbye my lover!'
-titleDiv.innerText=message;
-console.log('after: ',titleDiv.innerText)
-titleDiv.innerHTML=`<p>${message}</p>`
-titleDiv.style.color='red'
+// console.log('hello')
+// let titleDiv=document.getElementById('title')
+// console.log('before: ',titleDiv.innerText)
+// let message='Goodbye my lover!'
+// titleDiv.innerText=message;
+// console.log('after: ',titleDiv.innerText)
+// titleDiv.innerHTML=`<p>${message}</p>`
+// titleDiv.style.color='red'
 
-// let redDiv=document.getElementById('red')
-// let yellowDiv=document.getElementById('yellow')
-// let greenDiv=document.getElementById('green')
+// // let redDiv=document.getElementById('red')
+// // let yellowDiv=document.getElementById('yellow')
+// // let greenDiv=document.getElementById('green')
 
-// redDiv.onclick=()=>console.log('red')
-// yellowDiv.onclick=()=>console.log('yellow')
-// greenDiv.onclick=()=>console.log('green')
+// // redDiv.onclick=()=>console.log('red')
+// // yellowDiv.onclick=()=>console.log('yellow')
+// // greenDiv.onclick=()=>console.log('green')
+
+// const squares=document.querySelectorAll('.colorSquare')
+// const timesClicked={'red':0, 'yellow':0, 'green':0}
+// squares.forEach(square=>{
+//   square.onclick=()=>{
+//     timesClicked[square.value]+=1
+//     square.innerText=timesClicked[square.value]
+//   }
+// })
+// function clearScores(){
+//   timesClicked.red=0
+//   timesClicked.yellow=0
+//   timesClicked.green=0
+//   squares.forEach(square=>{
+//     square.innerText=''
+//   })
+// } 
+
+// const clearGameBtn=document.getElementById('clear-game')
+// clearGameBtn.onclick=()=>clearScores()
+
 
 const squares=document.querySelectorAll('.colorSquare')
-const timesClicked={'red':0, 'yellow':0, 'green':0}
+const timedClicked={'red':0, 'yellow':0, 'green':0}
 squares.forEach(square=>{
   square.onclick=()=>{
-    timesClicked[square.value]+=1
-    square.innerText=timesClicked[square.value]
+    timedClicked[square.value]+=1
+    square.innerText=timedClicked[square.value]
   }
 })
-function clearScores(){
-  timesClicked.red=0
-  timesClicked.yellow=0
-  timesClicked.green=0
+
+function clearGame(){
+  timedClicked.red=0
+  timedClicked.yellow=0
+  timedClicked.green=0
   squares.forEach(square=>{
     square.innerText=''
   })
-} 
-
+}
 const clearGameBtn=document.getElementById('clear-game')
-clearGameBtn.onclick=()=>clearScores()
+clearGameBtn.onclick=()=>clearGame()
